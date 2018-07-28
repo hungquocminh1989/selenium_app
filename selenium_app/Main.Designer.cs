3e749be69c15d737e7f9ade09d88df085aac085b
Minh <hungquocminh1989@gmail.com>
2018-07-28 16:38:57 +0700

    init source

---
+++ selenium_app/Main.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace selenium_app
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExec = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(214, 113);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(175, 76);
+            this.btnExec.TabIndex = 0;
+            this.btnExec.Text = "Run Browser";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 320);
+            this.Controls.Add(this.btnExec);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExec;
+    }
+}
+
---
+++ selenium_app/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using selenium_app.Library;
+
+namespace selenium_app
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            DriverConnector abc = new DriverConnector();
+            MessageBox.Show("aaaaaaaaaaaaa");
+        }
+    }
+}
